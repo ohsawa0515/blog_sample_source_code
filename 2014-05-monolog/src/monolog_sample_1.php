0a43e162804950152285fde01133497a1383be7b
ohsawa0515 <ohsawa0515@gmail.com>
2014-05-11 01:04:01 +0900

    ロギングするメソッドを変更

--- 2014-05-monolog/src/monolog_sample_1.php
+++ 2014-05-monolog/src/monolog_sample_1.php
@@ -15,20 +15,31 @@ $logger = new Logger('foo_test');
 $logger->pushHandler(new StreamHandler($logging_path, Logger::INFO));
 
 // デバッグレベルなのでロギングされない
-$logger->debug('debug_bar');
+// $logger->debug()と同じ
+$logger->addDebug('debug_bar');
 
 // 以下、すべてロギングされる
 // 下にいくほどログレベルが高くなる
-$logger->info('info_bar');
-$logger->notice('notice_bar');
-// $logger->warn()と同じ
-$logger->warning('warning_bar');
-// $logger->err()と同じ
-$logger->error('error_bar');
-// $logger->crit()と同じ
-$logger->critical('critical_bar');
-$logger->alert('alert_bar');
-// $logger->emerg()と同じ
-$logger->emergency('emergency_bar');
+
+// $logger->info()と同じ
+$logger->addInfo('info_bar');
+
+// $logger->notice()と同じ
+$logger->addNotice('notice_bar');
+
+// $logger->warning(), $logger->warn()と同じ
+$logger->addWarning('warning_bar');
+
+// $logger->error(), $logger->err()と同じ
+$logger->addError('error_bar');
+
+// $logger->critical(), $logger->crit()と同じ
+$logger->addCritical('critical_bar');
+
+// $logger->alert()と同じ
+$logger->addAlert('alert_bar');
+
+// $logger->emergency(), $logger->emerg()と同じ
+$logger->addEmergency('emergency_bar');
 
 exit;
